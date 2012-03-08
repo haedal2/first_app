d27f892ec29022e73256919bd9e553c75bcdf0e4
June Lee <june@kakao.com>
2012-03-08 11:40:12 +0900

    fisrt_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd6be0cf0e59ae00daad95dadd6c0aa8c33372e0a696c4bb748ba16dd793f2e7a15c10cbdbe181452a61b5bf8f065ef76124d3acbc9c3a6650973a9fdf498cd4f'
